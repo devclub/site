49c5c81eca35e688ea700d063f8e035882a66bb3
Anton Stalnuhhin <anton.stalnuhhin@nortal.com>
2018-03-25 14:50:13 +0300

    translated about, added ressource values into data context

--- src/app/components/ressources.ts
+++ src/app/components/ressources.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {DataContext} from '../data.context';
 
 @Component({
   selector: 'ressources',
   templateUrl: './ressources.html'
 })
 export class Ressources {
+  constructor(public dataContext: DataContext) {
+  }
 }
