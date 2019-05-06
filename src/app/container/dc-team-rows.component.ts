3057e0f329da9d16cec86658fdec4503647b74fe
anton.stalnuhhin <anton.stalnuhhin@nortal.com>
2019-05-06 16:13:46 +0300

    fixed link on member photo - also there is link on text

--- src/app/container/dc-team-rows.component.ts
+++ src/app/container/dc-team-rows.component.ts
@@ -3,7 +3,8 @@ import {Member} from '../data/models/member.model';
 
 @Component({
   selector: 'dc-team-rows',
-  templateUrl: './dc-team-rows.component.html'
+  templateUrl: './dc-team-rows.component.html',
+  styleUrls: ['./dc-team-rows.component.css']
 })
 export class DcTeamRowsComponent {
   @Input() public memberRows: Array<Member[]>;
