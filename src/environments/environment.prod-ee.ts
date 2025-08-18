18b04424340504d5ae59f048a64f16bee976eeaf
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-08-19 00:37:51 +0300

    load *.json files directly from https://devclub.github.io/data/
    These files are published automatically by GitHub itself (aka "GH Pages)". We don't need to run `git pull` on our server to get these changes anymore.

--- src/environments/environment.prod-ee.ts
+++ src/environments/environment.prod-ee.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  config: 'https://data.devclub.eu/ee_conf.json',
+  config: 'https://devclub.github.io/data/ee_conf.json',
   googleAnalyticsKey: 'UA-69567304-3'
 };
